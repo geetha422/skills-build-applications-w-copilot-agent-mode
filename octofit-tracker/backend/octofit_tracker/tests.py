c240e40e7a63b969c8a18774dad8ada0ef457476
geetha422 <konda.geetharchana@gds.ey.com>
2026-02-21 11:29:41 +0000

    Add initial Django project structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        team = Team.objects.create(name='Marvel', description='Marvel Team')
+        user = User.objects.create(name='Tony Stark', email='tony@stark.com', team=team)
+        self.assertEqual(user.email, 'tony@stark.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='DC', description='DC Team')
+        self.assertEqual(team.name, 'DC')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        team = Team.objects.create(name='Marvel', description='Marvel Team')
+        user = User.objects.create(name='Steve Rogers', email='steve@rogers.com', team=team)
+        activity = Activity.objects.create(user=user, type='Running', duration=30, date='2024-01-01')
+        self.assertEqual(activity.type, 'Running')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Upper body', difficulty='Easy')
+        self.assertEqual(workout.name, 'Pushups')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        team = Team.objects.create(name='Marvel', description='Marvel Team')
+        user = User.objects.create(name='Bruce Banner', email='bruce@banner.com', team=team)
+        leaderboard = Leaderboard.objects.create(user=user, score=100)
+        self.assertEqual(leaderboard.score, 100)
